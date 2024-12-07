4dc9d5d6ffcc5f601a50f45257fb09f60220eceb
Romi sinizkey <sinizromi@gmail.com>
2024-12-07 20:38:49 +0200

    Update Image.h

--- include/Image.h
+++ include/Image.h
@@ -7,9 +7,18 @@ public:
 	Image();
 	Image(int width, int hight);
 	Image(int width, int hight, Pixel pixel);
+	
+	bool operator==(const Image& other) const;
+	bool operator!=(const Image& other) const;
+	Image operator+(const Image& other)const  ;
+	Image& operator+=(const Image& other);
+
+
+
 
 private:
 	int W;
-	int H;
-	Pixel p;
+	int H ;
+	Pixel **pixels;
+	void allocatePixels(int width, int height, Pixel defaultPixel);
 };
