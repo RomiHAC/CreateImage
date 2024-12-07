eea96c9136a337135af5daa7cfc8804a45101b51
Romi sinizkey <sinizromi@gmail.com>
2024-12-07 13:17:01 +0200

    Update Pixel.cpp

--- src/Pixel.cpp
+++ src/Pixel.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "Pixel.h"
+
+Pixel::Pixel(unsigned char pixel)
+{
+	color = pixel;
+}
+
+bool Pixel::operator==(const Pixel& other) const
+{
+	return color == other.color;
+}
+
+bool Pixel::operator!=(const Pixel& other) const
+{
+	return  !(*this == other);
+}
+
+
+
+
 
