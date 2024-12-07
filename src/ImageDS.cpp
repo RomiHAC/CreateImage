9d48a1cc343021de3f168d5a5a981f4c775e9b31
Romi sinizkey <sinizromi@gmail.com>
2024-12-07 20:12:03 +0200

    Update ImageDS.cpp

--- src/ImageDS.cpp
+++ src/ImageDS.cpp
@@ -1,5 +1,7 @@
-#include "Pixel.h"
 #include <iostream>
+#include "ImageDS.h"
 
+ImageDS::ImageDS(): images(nullptr),count(0) {}
 
+ImageDS::ImageDS(Image* image, int amount) :images(image), count(amount) {}
 
