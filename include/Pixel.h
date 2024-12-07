35914a92eaa2b7365f7acfa5d26d9d7b1330dc50
Romi sinizkey <sinizromi@gmail.com>
2024-12-07 14:25:53 +0200

    Update Pixel.h

--- include/Pixel.h
+++ include/Pixel.h
@@ -6,13 +6,16 @@ class Pixel {
 public:
 	Pixel(unsigned char pixel = ' ');
 
-
-	const unsigned char BLACK = (unsigned char)219;
-	const unsigned char GRAY = (unsigned char)176;
-	const unsigned char WHITE = (unsigned char)32;
-	
 	bool operator==(const Pixel& other) const; 
 	bool operator!=(const Pixel& other) const;
+	Pixel operator|(const Pixel& other) const;
+	Pixel& operator|=(const Pixel& other);
+	Pixel operator&(const Pixel& other) const;
+
+	Pixel& operator&=(const Pixel& other);
+	
+
+	friend  std::ostream& operator<< (std::ostream& output, const Pixel& pixel);
 
 	
 
