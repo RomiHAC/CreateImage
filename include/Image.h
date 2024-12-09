2af76214f0895cf300b41184799d3d7305474d84
Romi sinizkey <sinizromi@gmail.com>
2024-12-09 13:07:55 +0200

    Update Image.h

--- include/Image.h
+++ include/Image.h
@@ -8,15 +8,22 @@ public:
 	Image();
 	Image(int width, int hight);
 	Image(int width, int hight, Pixel pixel);
+
+	~Image();
 	
 	bool operator==(const Image& other) const;
 	bool operator!=(const Image& other) const;
 	Image operator+(const Image& other)const  ;
 	Image& operator=(const Image& other);
 	Image& operator+=(const Image& other);
+	Image operator|(const Image& other) const;
+	Image& operator|=(const Image& other);
 
-	friend std::ostream& operator<<(std::ostream& output, const Image& temp_image);
 
+
+
+	int getHeight() const;
+	int getWidth() const;
 
 private:
 	int W;
@@ -24,3 +31,6 @@ private:
 	Pixel **pixels;
 	void allocatePixels(int width, int height, Pixel defaultPixel);
 };
+
+
+std::ostream& operator<<(std::ostream& output, const Image& temp_image);
