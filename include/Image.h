fb9fad0e9fa3227d6a53520ef90d7bd56e87e42e
Romi sinizkey <sinizromi@gmail.com>
2024-12-10 23:17:47 +0200

    Update Image.h

--- include/Image.h
+++ include/Image.h
@@ -5,33 +5,36 @@
 
 class Image {
 public:
-    Image();                                    
-    Image(int width, int height);               
-    Image(int width, int height, Pixel pixel);  
-    Image(const Image& other);                  
-    ~Image();                                  
+    Image();
+    Image(int width, int height);
+    Image(int width, int height, Pixel pixel);
+    Image(const Image& other);
+    ~Image();
 
-    Image operator+(const Image& other) const;  
+    Image operator+(const Image& other) const;
     //void operator=(const Image& other);
-    void operator+=(const Image& other);        
-    Image operator|(const Image& other) const;  
-    void operator|=(const Image& other);       
-    Image operator&(const Image& other) const;  
-    void operator&=(const Image& other);        
-    Image operator*(unsigned int N) const;      
-    Image& operator~();                         
+    void operator+=(const Image& other);
+    Image operator|(const Image& other) const;
+    void operator|=(const Image& other);
+    Image operator&(const Image& other) const;
+    void operator&=(const Image& other);
 
-    const Pixel operator()(unsigned int X, unsigned int Y) const; 
-    Pixel& operator()(unsigned int X, unsigned int Y);           
+    Image operator*(unsigned int N) const;
+    Image& operator~();
 
-    int GetHeight() const;                    
-    int GetWidth() const;                     
+    const Pixel operator()(unsigned int X, unsigned int Y) const;
+    Pixel& operator()(unsigned int X, unsigned int Y);
+
+    int GetHeight() const;
+    int GetWidth() const;
 
 private:
-    int W;             
-    int H;             
-    ImageDS* DSimg;      
+    int W;
+    int H;
+    ImageDS* DSimg;
 };
+Image operator*(unsigned int N, const Image& original);
+void operator*=(Image& original, unsigned int N);
 
 // lso / rso = Left side operator / Right side operator
 bool operator==(const Image& lso, const Image& rso);
