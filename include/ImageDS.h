a0bf1e1be84153d79ad11c3b85e51acd23ec38be
The4ing <atar.orram101@gmail.com>
2024-12-18 17:38:06 +0200

    Update ImageDS.h

--- include/ImageDS.h
+++ include/ImageDS.h
@@ -8,23 +8,18 @@ public:
     ImageDS(int height, int width, Pixel pixel);
     ImageDS(const ImageDS& other);
     ~ImageDS();
-
-    //  ImageDS& operator=(const ImageDS& other);
-
+    ImageDS& operator=(const ImageDS& other);
     Pixel getPixel(int x, int y) const;
     Pixel& getPixel(int x, int y);
     void setPixel(int x, int y, const Pixel& pixel);
 
     int getWidth() const;
     int getHeight() const;
-    void setHeight(int H);
-    void setWidth(int W);
 
 private:
     int width;
     int height;
     Pixel** pixels;
-
     void allocatePixels(int W, int H);
     void deallocatePixels();
 };
