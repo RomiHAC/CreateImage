373efc2a0e09ec808143f59fb5d4f8f6801d580a
The4ing <atar.orram101@gmail.com>
2024-12-18 17:40:21 +0200

    Update Pixel.h

--- include/Pixel.h
+++ include/Pixel.h
@@ -9,34 +9,17 @@ private:
     unsigned char color;
 
 public:
-
-
-    // Constructor
     Pixel(unsigned char pixel = WHITE);
-    Pixel(const Pixel& other);
-    // Logical operators
-    //Pixel operator|(const Pixel& other) const;
-
-
-    //Pixel operator&(const Pixel& other) const;
-
+    Pixel(unsigned char pixel);
 
     Pixel& operator=(const Pixel& other);
-
-    //friend std::ostream& operator<<(std::ostream& os, const Pixel& pixel);
-
-    unsigned char getColor() const; // Updated to return unsigned char
+    unsigned char getColor() const; 
 };
 
-// Comparison operators
 bool operator==(const Pixel& first, const Pixel& other);
 bool operator!=(const Pixel& first, const Pixel& other);
-
-// Stream output
-std::ostream& operator<<(std::ostream& output, const Pixel& pixel);
-
+std::ostream& operator<<(std::ostream& output, const Pixel& pixel); 
 Pixel operator|(const Pixel& original, const Pixel& other);
 Pixel& operator|=(Pixel& original, const Pixel& other);
-
 Pixel operator&(const Pixel& original, const Pixel& other);
 Pixel& operator&=(Pixel& original, const Pixel& other);
