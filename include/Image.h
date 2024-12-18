30d5baacfe9ed12800ae4d5a21e95b0e41eb8d26
The4ing <atar.orram101@gmail.com>
2024-12-18 17:37:36 +0200

    Update Image.h

--- include/Image.h
+++ include/Image.h
@@ -5,64 +5,28 @@
 
 class Image {
 public:
-    Image();                                    // Default Constructor
-    Image(int height, int width);              // Constructor with dimensions
-    Image(int height, int width, Pixel pixel); // Constructor with dimensions and default pixel
-    Image(const Image& other);                 // Copy Constructor
-    ~Image();                                  // Destructor
-
-
-   // bool operator==(const Image& other) const ;
-
-    //Image operator+(const Image& other) const; // Concatenation operator
-
-    //Image operator|(const Image& other) const; // Union operator
-
-    //Image operator&(const Image& other) const; // Intersection operator
-
-
-    //Image operator*(unsigned int N) const;     // Repeat operator
-
-   // Image& operator~();                        // Negation operator
-   // Image& operator=(const Image& other);      // Assignment Operator
-    Image& operator=(const Image& other);
+    Image();                                 
+    Image(int height, int width);              
+    Image(int height, int width, Pixel pixel); 
     const Pixel operator()(unsigned int X, unsigned int Y) const; // Access pixel (const)
     Pixel& operator()(unsigned int X, unsigned int Y);            // Access pixel (non-const)
-
-    // friend std::ostream& operator<<(std::ostream& os, const  Image& image);
-
-    int GetHeight() const;                     // Get image height
-    int GetWidth() const;                      // Get image width
+    int GetHeight() const;                 
+    int GetWidth() const;                  
 
 private:
-    //int W;             // Width of the image
-  //  int H;             // Height of the image
-    ImageDS* DSimg;    // Pointer to the underlying data structure
+    ImageDS DSimg;
 };
-
-// Comparison operators
-
 
 bool operator==(const Image& lhs, const Image& rhs); // check if equal
 bool operator!=(const Image& lhs, const Image& rhs);// check if not equal
-
-
-
 Image operator+(const Image& original, const Image& other);
 Image& operator+=(Image& original, const Image& other); // Compound concatenation operator
-
 Image operator|(const Image& original, const Image& other); // Union operator
 Image& operator|=(Image& original, const Image& other); //Compound Union operator
-
 Image operator&(const Image& original, const Image& other);  // Intersection operator
 Image& operator&=(Image& original, const Image& other);       // Compound intersection operator
-
 Image operator*(const Image& original, unsigned int N);     // Repeat operator
 Image operator*(unsigned int N, const Image& original);// Repeat operator
 Image& operator*=(Image& original, unsigned int N);// Compound Repeat operator
-
 Image& operator~(Image& original);                        // Negation operator
-
-
-// Output stream operator
-std::ostream& operator<<(std::ostream& os, const Image& image); 
+std::ostream& operator<<(std::ostream& os, const Image& image); // Output stream operator
